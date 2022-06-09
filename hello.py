908b67f86883fb16c48f0e0658b738f98b962aae
Disha-Patil <dishapatil10@gmail.com>
2022-06-09 12:37:53 +0530

    flask hello world

---
+++ hello.py
@@ -0,0 +1,13 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+# @app.route("/hello")
+# def hello_world():
+#     return "<p>Hello, World!</p>"
+
+from markupsafe import escape
+
+@app.route("/<name>")
+def hello(name):
+    return f"Hello, {escape(name)}!"
